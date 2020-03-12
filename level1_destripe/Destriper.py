Updated destriping code, fixed off by one error, seems stable at this time. No multi-threading.

level1_destripe/Destriper.py
@@ -29,7 +29,7 @@ def Destriper(parameters, data):
 
     return offsetMap, offsets
 
-def CGM(data, offsets, offsetMap, niter=200):
+def CGM(data, offsets, offsetMap, niter=400):
     """
     Conj. Gradient Inversion
     """
@@ -40,85 +40,79 @@ def CGM(data, offsets, offsetMap, niter=200):
     b  = data.residual
     counts = offsets.offsets*0.
 
+    b.average()
+    Ax.average()
 
+    # Estimate initial residual
     binFuncs.EstimateResidual(Ax.offsets, # Holds the weighted residuals
                               counts,
                               offsets.offsets, # holds the target offsets
-                              b.wei, # The weights calculated from the data
+                              #b.wei, # The weights calculated from the data
+                              data.allweights,#residual.wei,
                               offsetMap.output, # Map to store the offsets in (initially all zero)
                               offsets.offsetpixels, # Maps offsets to TOD position
                               data.pixels) # Maps pixels to TOD position
-    #Ax.offsets /= counts
+
+
     print('Diag counts:',np.min(counts))
-    #r = (offsets.offsets[offsets.offsetpixels]-offsetMap.output[data.pixels])#*data.weights
-    #binFuncs.binValues(Ax.offsets, 
-    #                   offsets.offsetpixels, 
-    #                   weights=r)
-    
+
 
     #Ax.offsets = Ax.offsets/counts #* offsets.offset
     # -- Calculate the initial residual and direction vectors
     #b.sigwei *= offsets.offset
     print('Diags b.sigwei, Ax.offsets:', np.sum(b.sigwei), np.sum(Ax.offsets))
+
     residual = b.sigwei - Ax.offsets
-    r2 = b.sigwei - Ax.offsets
     direction= b.sigwei - Ax.offsets
+
+    r2 = b.sigwei - Ax.offsets
+
     # -- Initial threshhold
     thresh0 = np.sum(residual**2)
-    dnew = np.sum(residual**2)
-    alpha = 0
+    dnew    = np.sum(residual**2)
+    alpha   = 0
 
     print('Diags thresh0:', thresh0)
-
     #offsets.offsets = data.residual.offsets 
     lastoffset = 0
+    newVals = np.zeros(niter)
+    alphas  = np.zeros(niter)
+
     for i in range(niter):
         # -- Calculate conjugate search vector Ad
         lastoffset = Ax.offsets*1.
         Ax.offsets *= 0
         counts *= 0
+
+        offsetMap.clearmaps()
+        offsetMap.binOffsets(direction,
+                             data.residual.wei,
+                             offsets.offsetpixels,
+                             data.pixels)
+        offsetMap.average()
+
         binFuncs.EstimateResidual(Ax.offsets,
                                   counts,
                                   direction,
-                                  data.residual.wei,
+                                  data.allweights,#residual.wei,
                                   offsetMap.output,
                                   offsets.offsetpixels,
                                   data.pixels)
-        # Ax.offsets /= counts
-        # Axcopy = Ax.offsets*1.
-        # Ax.offsets *= 0
 
-        # r = (direction[offsets.offsetpixels]-offsetMap.output[data.pixels])#*weights
-        # binFuncs.binValues(Ax.offsets, 
-        #                   offsets.offsetpixels, 
-        #                   weights=r)
-
-        # pyplot.subplot(211)
-        # pyplot.plot(Axcopy)
-        # pyplot.subplot(212)
-        # pyplot.plot(Ax.offsets)
-        # pyplot.show()
-
-        print('Diags (Ax.offsets (1)):',np.sum(Ax.offsets))
-
+                         
         
-        #Ax.offsets = Ax.offsets/counts # * offsets.offset
-        #pyplot.subplot(211)
-        #pyplot.plot(Ax.offsets)
-        #pyplot.plot(lastoffset)
-        #pyplot.subplot(212)
-        #pyplot.plot(counts)
-        #pyplot.show()
+
         # Calculate the search vector
         dTq = np.sum(direction*Ax.offsets)
 
         # 
         alpha = dnew/dTq
-
+        alphas[i]=alpha
         # -- Update offsets
 
         olfast = offsets.offsets*1.
         offsets.offsets += alpha*direction
+        #offsets.offsets[0] = offsets.offsets[1]
 
         # -- Calculate new residual
         if np.mod(i,50) == 0:
@@ -134,26 +128,20 @@ def CGM(data, offsets, offsetMap, niter=200):
             binFuncs.EstimateResidual(Ax.offsets, # Holds the weighted residuals
                                       counts,
                                       offsets.offsets, # holds the target offsets
-                                      b.wei, # The weights calculated from the data
+                                      #b.wei, # The weights calculated from the data
+                                      data.allweights,#residual.wei,
                                       offsetMap.output, # Map to store the offsets in (initially all zero)
                                       offsets.offsetpixels, # Maps offsets to TOD position
                                       data.pixels) # Maps pixels to TOD position
-            #Ax.offsets /= counts
 
-
-            # r = (offsets.offsets[offsets.offsetpixels]-offsetMap.output[data.pixels])#*weights
-            # binFuncs.binValues(Ax.offsets, 
-            #                    offsets.offsetpixels, 
-            #                    weights=r)
             residual = b.sigwei - Ax.offsets
         else:
             residual = residual -  alpha*Ax.offsets 
-
-        print('Diag residual:' , np.sum(residual))
+        #print('Diag residual:' , np.sum(residual))
 
         dold = dnew*1.0
         dnew = np.sum(residual**2)
-
+        newVals[i] = dnew
         # --
         beta = dnew/dold
 
@@ -167,9 +155,24 @@ def CGM(data, offsets, offsetMap, niter=200):
                              data.pixels)
         offsetMap.average()
                    
-        print((-np.log10(dnew/thresh0))/6 )
-        if dnew/thresh0 < 1e-6:
+        
+
+        #print((-np.log10(dnew/thresh0))/8 )
+        if dnew/thresh0 < 1e-8:
             break
+    if False:
+        pyplot.subplot(221)
+        pyplot.plot(newVals)
+        pyplot.yscale('log')
+        pyplot.xscale('log')
+        pyplot.grid()
+        pyplot.subplot(222)
+        pyplot.plot(alphas)
+        pyplot.yscale('log')
+        pyplot.xscale('log')
+        pyplot.grid()
+        pyplot.show()
+    print('Achieved {} in {} steps'.format(dnew/thresh0, i))
 
     offsetMap.clearmaps()
     offsetMap.binOffsets(offsets.offsets,

level1_destripe/Types.py
@@ -4,6 +4,7 @@ from astropy import wcs
 from matplotlib import pyplot
 from tqdm import tqdm
 import pandas as pd
+from scipy import linalg as la
 
 import binFuncs
 
@@ -12,7 +13,7 @@ class Data:
     """
     Reads in the TOD, stores it immediate in a naive map
     """
-    def __init__(self, parameters):
+    def __init__(self, parameters, frequency=0, band=0):
         
         self.nmodes = 5
         # -- constants
@@ -22,7 +23,8 @@ class Data:
         self.Nsamples = 0
         self.Nhorns = 0
         self.Nbands = 4
-        self.band = 0
+        self.band = int(band)
+        self.frequency = int(frequency)
         self.keeptod = False
         # -- read data
         filelist = np.loadtxt(parameters['Inputs']['filelist'],dtype=str,ndmin=1)
@@ -165,6 +167,8 @@ class Data:
         nSteps = tod.shape[1]//step
         templates = np.ones((step,2))
 
+        #nu = np.fft.fftfreq(tod.shape[1], d=1/50)
+        #ps1 = np.abs(np.fft.fft(tod[0,:]))**2
         for i in range(nSteps):
             lo = i*step
             if i < (nSteps - 1):
@@ -173,17 +177,63 @@ class Data:
                 hi = tod.shape[1]
                 templates = np.ones((hi - lo,2))
 
-            templates[:,0] =np.median(tod[:,lo:hi],axis=0)# medfilt(np.median(tod[:,lo:hi],axis=0),51)
-            
-            C = templates.T.dot(templates)
-            Cinv = inv(C)
-            a = Cinv.dot(templates.T.dot(tod[:,lo:hi].T))
-            a = a.T
-            mdl = np.sum(templates[:,np.newaxis,:]*a[np.newaxis,:,:],axis=-1).T
+            #templates[:,0] = medfilt(np.median(tod[:,lo:hi],axis=0),151)
+            # 
             
             #print(mdl.shape, tod[:,lo:hi].shape)
             
-            tod[:,lo:hi] -= mdl
+            medfilt_tod = np.zeros((tod.shape[0],hi-lo))
+            binDown = 5
+            binCount = int((hi-lo)/binDown +0.5)
+            if binCount*binDown < tod.shape[1]:
+                binCount += 1
+            binEdges = np.linspace(0,hi-lo, binCount+1)
+            positions = np.arange(hi-lo)
+            w = np.histogram(positions, binEdges)[0]
+            for feed in range(tod.shape[0]):
+               s = np.histogram(positions, binEdges, weights=tod[feed,lo:hi])[0]
+               #print(s.size*binDown, hi-lo)
+               medfilt_tod[feed,:] = np.repeat(s/w, binDown)[:hi-lo]
+                ##medfilt(tod,501)
+
+            #C = medfilt_tod.dot(medfilt_tod.T)
+            #C = tod[:,lo:hi].dot(tod[:,lo:hi].T)
+            #U, s, Vh = la.svd(medfilt_tod, full_matrices=False)
+            #print(U.shape, templates.shape, s.shape)
+            #val, vec = linalg.eigh(C)
+            #templates = Vh[0:1,:].T
+            #C = templates.T.dot(templates)
+            #Cinv = inv(C)
+            #a = Cinv.dot(templates.T.dot(tod[:,lo:hi].T))
+            #a = a.T
+            #mdl = np.sum(templates[:,np.newaxis,:]*a[np.newaxis,:,:],axis=-1).T
+            #icut = 5
+            #mdl = U[:,:icut].dot(np.diag(s)[:icut,:].dot(Vh[:,:]))
+
+            #for feed in range(Vh.shape[0]):
+            #    pyplot.plot(tod[feed,lo:hi])
+            #    pyplot.plot(mdl[feed,:])
+            #    pyplot.show()
+            #pyplot.subplot(211)
+            #pyplot.imshow(C)
+            #pyplot.subplot(212)
+            #pyplot.imshow(C2)
+            #pyplot.show()
+            #pyplot.plot(tod[0,:])
+            #pyplot.plot(mdl[0,:])
+            #pyplot.plot(Vh[0,:])
+            #pyplot.show()
+            #pyplot.plot(tod[0,lo:hi])
+            #pyplot.plot(medfilt_tod[0,:])
+            #pyplot.show()
+            tod[:,lo:hi] -= medfilt_tod #mdl
+        #ps2 = np.abs(np.fft.fft(tod[0,:]))**2
+        #pyplot.plot(nu[1:nu.size//2], ps1[1:nu.size//2])
+        #pyplot.plot(nu[1:nu.size//2], ps2[1:nu.size//2])
+        #pyplot.yscale('log')
+        #pyplot.xscale('log')
+        #pyplot.grid()
+        #pyplot.show()
         return tod
        # pyplot.plot(tod[0,:])
         #pyplot.show()
@@ -315,12 +365,33 @@ class Data:
 
         self.residual.accumulate(tod,weights,self.naive.output,self.pixels,self.chunks[i])
 
+    def skyPixels(self,i, d,Feeds, selectFeature):
+        """
+        Returns the pixel coordinates in the WCS frame
+        """
+
+        # We store all the pointing information
+        x  = (d['level1/spectrometer/pixel_pointing/pixel_ra'][...])[Feeds,selectFeature]
+        x  = x[...,0:self.datasizes[i]].flatten()
+        y  = (d['level1/spectrometer/pixel_pointing/pixel_dec'][...])[Feeds,selectFeature]
+        y  = y[...,0:self.datasizes[i]].flatten()
+
+
+        el  = (d['level1/spectrometer/pixel_pointing/pixel_el'][...])[Feeds,selectFeature]
+        el  = el[...,0:self.datasizes[i]]
+
+
+        pixels = self.getFlatPixels(x,y)
+        pixels[pixels < 0] = -1
+        pixels[pixels > self.naive.npix] = -1
+
+        return pixels
 
 class DataLevel2(Data):
-    def __init__(self, parameters):
+    def __init__(self, parameters, band=0, frequency=0,keeptod=False):
         
         self.nmodes = 5
-        # -- constants
+        # -- constants -- a lot of these are COMAP specific
         self.ifeature = 5
         self.chunks = []
         self.datasizes = []
@@ -328,21 +399,10 @@ class DataLevel2(Data):
         self.Nhorns = 0
         self.Nbands = 4
 
-        self.band = 0
-        self.frequency = 0
+        self.band = int(band)
+        self.frequency = int(frequency)
 
-        self.keeptod = False
-        # -- read data
-        filelist = np.loadtxt(parameters['Inputs']['filelist'],dtype=str,ndmin=1)
-
-        # Setup the map coordinates
-        self.crval = parameters['WCS']['crval']
-        self.cdelt = [cd/60. for cd in parameters['WCS']['cdelt']]
-        self.crpix = parameters['WCS']['crpix']
-        self.ctype = parameters['WCS']['ctype']
-        self.nxpix = int(self.crpix[0]*2)
-        self.nypix = int(self.crpix[1]*2)
-        self.setWCS(self.crval, self.cdelt, self.crpix, self.ctype)
+        self.keeptod = keeptod
 
         self.offsetLen = parameters['Destriper']['offset']
 
@@ -354,34 +414,66 @@ class DataLevel2(Data):
             self.Feeds = [int(self.Feeds)]
             self.Nfeeds = 1
 
+
+        # Setup the map coordinates -- Needed for generating pixel coordinates
+        self.crval = parameters['WCS']['crval']
+        self.cdelt = [cd/60. for cd in parameters['WCS']['cdelt']]
+        self.crpix = parameters['WCS']['crpix']
+        self.ctype = parameters['WCS']['ctype']
+        self.nxpix = int(self.crpix[0]*2)
+        self.nypix = int(self.crpix[1]*2)
+        self.setWCS(self.crval, self.cdelt, self.crpix, self.ctype)
+
+        self.naive  = Map(self.nxpix,self.nypix, self.wcs)
+        self.hits   = Map(self.nxpix,self.nypix, self.wcs)
+
+        # -- read data
+        filelist = np.loadtxt(parameters['Inputs']['filelist'],dtype=str,ndmin=1)
+
+        # Will define Nsamples, datasizes[], and chunks[[]]
         for filename in filelist:
             self.countDataSize(filename)
 
-        Noffsets  = self.Nsamples//self.offsetLen
-
         self.pixels = np.zeros(self.Nsamples,dtype=int)
 
+        # If we want to keep all the TOD samples for plotting purposes...
         if self.keeptod:
             self.todall = np.zeros(self.Nsamples)
+        self.allweights = np.zeros(self.Nsamples)
 
 
-        self.naive  = Map(self.nxpix,self.nypix, self.wcs)
+
+        # First read in all the data
+        # Remember we want to solve Ax = b,
+        # "b" contains all the data, so we construct that now:
+        # 1a) Create a naive binned map
+        # 1b) Sum all the data into offsets
+        # 2) Subtract the naive weighted map from the offsets
+        # "b" residual vector is saved in residual Offset object
+        Noffsets  = self.Nsamples//self.offsetLen
+        self.residual = Offsets(self.offsetLen, Noffsets, self.Nsamples)
+
         for i, filename in enumerate(tqdm(filelist)):
             self.readData(i,filename)
-
-
         self.naive.average()
-        # -- Finally create the noise offset residual
-        self.residual = Offsets(self.offsetLen, Noffsets, self.Nsamples)
-        for i, filename in enumerate(tqdm(filelist)):
-            self.offsetResidual(i,filename)
+        self.residual.accumulate(-self.naive.output[self.pixels],self.allweights,[0,self.pixels.size])
+
         self.residual.average()
+
+
+
+        # -- Finally create the noise offset residual
+        #for i, filename in enumerate(tqdm(filelist)):
+        #    self.offsetResidual(i,filename)
+        #self.residual.average()
         
 
 
     def countDataSize(self,filename):
         """
-        Get size of data for this file
+        Opens each datafile and determines the number of samples
+
+        Uses the features to select the correct chunk of data
         """
         
         d = h5py.File(filename,'r')
@@ -418,47 +510,51 @@ class DataLevel2(Data):
         # --- Feed position indices can change
         Feeds = self.GetFeeds(d['level1/spectrometer/feeds'][...], self.Feeds)
 
-        # We store all the pointing information
-        x  = (d['level1/spectrometer/pixel_pointing/pixel_ra'][...])[Feeds,selectFeature]
-        x  = x[...,0:self.datasizes[i]].flatten()
-        y  = (d['level1/spectrometer/pixel_pointing/pixel_dec'][...])[Feeds,selectFeature]
-        y  = y[...,0:self.datasizes[i]].flatten()
-
+        self.pixels[self.chunks[i][0]:self.chunks[i][1]] = self.skyPixels(i, d,Feeds, selectFeature)
         el  = (d['level1/spectrometer/pixel_pointing/pixel_el'][...])[Feeds,selectFeature]
         el  = el[...,0:self.datasizes[i]]
 
-
-        pixels = self.getFlatPixels(x,y)
-        pixels[pixels < 0] = -1
-        pixels[pixels > self.naive.npix] = -1
-        self.pixels[self.chunks[i][0]:self.chunks[i][1]] = pixels
-        
         # Now accumulate the TOD into the naive map
         tod = d['level2/averaged_tod'][Feeds,...]
         nFeeds, nBands, nChannels, nSamples = tod.shape
+
+        #todbig = np.reshape(tod,(nFeeds*nBands*nChannels, nSamples))
+
+        
+
+        print('check badn/freq', self.band, self.frequency)
         tod = tod[:,self.band,self.frequency,selectFeature]
-        tod  = tod[...,0:self.datasizes[i]]
+        tod = tod[...,0:self.datasizes[i]]
+
+        # Replace with simulated data
+        if False:
+            x  = (d['level1/spectrometer/pixel_pointing/pixel_ra'][...])[Feeds,selectFeature]
+            x  = x[...,0:self.datasizes[i]].flatten()
+            y  = (d['level1/spectrometer/pixel_pointing/pixel_dec'][...])[Feeds,selectFeature]
+            y  = y[...,0:self.datasizes[i]].flatten()
+            tod[...] = np.random.normal(size=tod.shape)
+            gauss2d = lambda P,x,y: P[0] * np.exp(-0.5*((x-P[1])**2 + (y-P[2])**2)/P[3]**2)
+            xmid, ymid = np.mean(x),np.mean(y)
+            todshape = tod.shape 
+            tod = tod.flatten()
+            tod += gauss2d([50,xmid,ymid,5/60.], x,y)
+            tod = np.reshape(tod, todshape)
 
 
-        t = np.arange(tod.shape[-1])
         weights = np.ones(tod.shape)
-        print(tod.shape, el.shape, nSamples,d['level1/spectrometer/pixel_pointing/pixel_el'].shape)
+        t = np.arange(tod.shape[-1])
         for j, feed in enumerate(Feeds):
-            #pyplot.plot(np.abs(tod[j,:N:2]-tod[j,1:N:2]))
-            ##pyplot.axhline(rms*5,color='r')
-            #pyplot.show()
+
             bad = np.isnan(tod[j,:])
+            if all(bad):
+                continue
             tod[j,bad] = np.interp(t[bad], t[~bad], tod[j,~bad])
             pmdl = np.poly1d(np.polyfit(1./np.sin(el[j,:]*np.pi/180.), tod[j,:],1))
             tod[j,:] -= pmdl(1./np.sin(el[j,:]*np.pi/180.))
             tod[j,:] -= np.nanmedian(tod[j,:])
-            #print(self.Feeds, feed,len(self.Feeds))
-            #print(feed, ifeed,caldata[feed[0],0])
-            ifeed = np.where((caldata[:,0] == self.Feeds[j]))[0]
-            tod[j,:] *= caldata[ifeed,1 + self.frequency + self.band*nChannels]
 
-            #pyplot.plot(tod[j,:])
-            #pyplot.show()
+            ifeed = np.where((caldata[:,0] == self.Feeds[j]))[0]
+
             N = tod.shape[1]//2 * 2
             diffTOD = tod[j,:N:2]-tod[j,1:N:2]
             rms = np.sqrt(np.nanmedian(diffTOD**2)*1.4826)
@@ -466,38 +562,14 @@ class DataLevel2(Data):
 
             # Remove spikes
             select = np.where((np.abs(diffTOD) > rms*5))[0]*2
-            #pyplot.plot(tod[j,:],',')
-            #pyplot.plot(select,tod[j,select],'.')
-            #pyplot.show()
             weights[j,select] *= 1e-10
+            print('Horn', self.Feeds[j], rms)
 
-            
-            print('Horn', j, rms)
-        tod = self.processChunks(tod, step=10000)
+        #tod = self.processChunks(tod, step=10000)
         rms = np.sqrt(np.nanmedian(tod**2,axis=1))*1.4826
         for j, feed in enumerate(Feeds):
             select = np.abs(tod[j,:]) > rms[j]*10
             weights[j,select] *= 1e-10
-
-        #pyplot.plot(tod[0,:]*weights[0,:])
-        #pyplot.show()
-        #from scipy.signal import medfilt
-        #bkgd = medfilt(np.nanmedian(tod,axis=0),kernel_size=401)
-        #tod -= bkgd[np.newaxis,:]
-        # pyplot.subplot(2,2,1)
-        # pyplot.plot(tod[0,:])
-        # pyplot.plot(bkgd)
-        # pyplot.subplot(2,2,2)
-        # pyplot.plot(tod[-1,:])
-        # pyplot.plot(bkgd)
-        # pyplot.subplot(2,2,3)
-        # pyplot.plot(tod[-5,:])
-        # pyplot.plot(bkgd)
-        # pyplot.subplot(2,2,4)
-        # pyplot.plot(tod[3,:])
-        # pyplot.plot(bkgd)
-
-        # pyplot.show()
 
         weights = weights.flatten()
         tod = tod.flatten()
@@ -508,81 +580,12 @@ class DataLevel2(Data):
         if self.keeptod:
             self.todall[self.chunks[i][0]:self.chunks[i][1]] = tod*1.
 
-
-        self.naive.accumulate(tod,weights,pixels)
-        self.hits.accumulatehits(pixels)
-        #self.residual.accumulate(tod,weights,self..output,self.pixels,self.chunks[i])
-
-    def offsetResidual(self, i, filename):
-        """
-        Reads data
-        """
-        gdir = '/scratch/nas_comap1/sharper/COMAP/runcomapreduce/AncillaryData/SourceFitsAllFeeds/'
-        calfile = 'CalFactors_Nov2019_Jupiter.dat'
-        caldata = np.loadtxt('{}/{}'.format(gdir,calfile))
-
-        d = h5py.File(filename,'r')
-
-        # -- Only want to look at the observation data
-        features = d['level1/spectrometer/features'][:]
-        selectFeature = self.featureBits(features.astype(float), self.ifeature)
-        features = features[selectFeature]
-
-        # --- Feed position indices can change
-        Feeds = self.GetFeeds(d['level1/spectrometer/feeds'][...], self.Feeds)
-
-
-        el  = (d['level1/spectrometer/pixel_pointing/pixel_el'][...])[Feeds,selectFeature]
-        el  = el[...,0:self.datasizes[i]]
-
-        # Now accumulate the TOD into the naive map
-        tod = d['level2/averaged_tod'][Feeds,...]
-        nFeeds, nBands, nChannels, nSamples = tod.shape
-        tod = tod[:,self.band,self.frequency,selectFeature]
-        tod  = tod[...,0:self.datasizes[i]]
-
-        t = np.arange(tod.shape[-1])
-        weights = np.ones(tod.shape)
-        for j, feed in enumerate(Feeds):
-            bad = np.isnan(tod[j,:])
-            tod[j,bad] = np.interp(t[bad], t[~bad], tod[j,~bad])
-            pmdl = np.poly1d(np.polyfit(1./np.sin(el[j,:]*np.pi/180.), tod[j,:],1))
-            tod[j,:] -= pmdl(1./np.sin(el[j,:]*np.pi/180.))
-            tod[j,:] -= np.nanmedian(tod[j,:])
-
-            ifeed = np.where((caldata[:,0] == self.Feeds[j]))[0]
-            tod[j,:] *= caldata[ifeed,1 + self.frequency + self.band*nChannels]
-
-            N = tod.shape[1]//2 * 2
-            diffTOD = tod[j,:N:2]-tod[j,1:N:2]
-            rms = np.sqrt(np.nanmedian(diffTOD**2)*1.4826)
-            weights[j,:] *= 1./rms**2
-
-            # Remove spikes
-            weights[j,np.where((np.abs(diffTOD) > rms*5))[0]*2] *= 1e-10
-
-
-
-            print('Horn', j, rms)
-
-        tod = self.processChunks(tod, step=10000)
-        rms = np.sqrt(np.nanmedian(tod**2,axis=1))*1.4826
-        for j, feed in enumerate(Feeds):
-            select = np.abs(tod[j,:]) > rms[j]*10
-            weights[j,select] *= 1e-10
+        self.allweights[self.chunks[i][0]:self.chunks[i][1]] = weights
         
-        #from scipy.signal import medfilt
-        #bkgd = medfilt(np.nanmedian(tod,axis=0),kernel_size=401)
-        #tod -= bkgd[np.newaxis,:]
+        self.naive.accumulate(tod,weights,self.pixels[self.chunks[i][0]:self.chunks[i][1]])
+        self.hits.accumulatehits(self.pixels[self.chunks[i][0]:self.chunks[i][1]])
+        self.residual.accumulate(tod,weights,self.chunks[i])
 
-
-        weights = weights.flatten()
-        tod = tod.flatten()
-        bad = (np.isnan(tod)) | (self.pixels[self.chunks[i][0]:self.chunks[i][1]] == -1)
-        tod[bad] = 0
-        weights[bad] = 0
-
-        self.residual.accumulate(tod,weights,self.naive.output,self.pixels,self.chunks[i])
 
 class DataSim(Data):
     def __init__(self, parameters):
@@ -709,8 +712,6 @@ class DataSim(Data):
         weights = np.ones(tod.shape)
         print(tod.shape, el.shape, nSamples,d['spectrometer/pixel_pointing/pixel_el'].shape)
         for j, feed in enumerate(Feeds):
-            pyplot.plot(tod[j,:])
-            pyplot.show()
             bad = np.isnan(tod[j,:])
             tod[j,bad] = np.interp(t[bad], t[~bad], tod[j,~bad])
             pmdl = np.poly1d(np.polyfit(1./np.sin(el[j,:]*np.pi/180.), tod[j,:],1))
@@ -855,7 +856,7 @@ class Map:
         Add more data to the naive map
         """
         binFuncs.binValues2Map(self.sigwei, pixels, offsets*weights, offsetpixels)
-        binFuncs.binValues2Map(self.wei, pixels, weights, offsetpixels)
+        binFuncs.binValues2Map(self.wei   , pixels, weights        , offsetpixels)
 
 
 
@@ -896,7 +897,6 @@ class Offsets:
         self.wei    = np.zeros(self.Noffsets)
 
         self.offsetpixels = np.arange(self.Nsamples)//self.offset
-        #self.offsetpixels[self.offsetpixels >= self.Noffsets] = self.Noffsets-1
 
     def __getitem__(self,i):
         """
@@ -908,12 +908,13 @@ class Offsets:
         return np.repeat(self.offsets, self.offset)[:self.Nsamples]
 
 
-    def accumulate(self,tod,weights,skymap,pixels,chunk):
+    def accumulate(self,tod,weights,chunk):
         """
         Add more data to residual offset
         """
-        binFuncs.binValues(self.sigwei, self.offsetpixels[chunk[0]:chunk[1]], weights=(tod-skymap[pixels[chunk[0]:chunk[1]]])*weights )
+        binFuncs.binValues(self.sigwei, self.offsetpixels[chunk[0]:chunk[1]], weights=tod*weights )
         binFuncs.binValues(self.wei   , self.offsetpixels[chunk[0]:chunk[1]], weights=weights    )
+
 
     def average(self):
         self.goodpix = np.where((self.wei != 0 ))[0]

level1_destripe/level1_destripe.py
@@ -18,13 +18,23 @@ from Types import *
 
 from Destriper import Destriper
 
+import ast
+class PythonLiteralOption(click.Option):
+
+    def type_cast_value(self, ctx, value):
+        try:
+            return ast.literal_eval(value)
+        except:
+            raise click.BadParameter(value)
+
 
 @click.command()
 @click.argument('filename')#, help='Level 1 hdf5 file')
-def call_level1_destripe(filename):
-    level1_destripe(filename)
+@click.option('--options', cls=PythonLiteralOption, default="{}")
+def call_level1_destripe(filename, options):
+    level1_destripe(filename, options)
 
-def level1_destripe(filename):
+def level1_destripe(filename,options):
     
     """Plot hit maps for feeds
 
@@ -36,41 +46,60 @@ def level1_destripe(filename):
     # Get the inputs:
     parameters = ParserClass.Parser(filename)
 
+    for k1,v1 in options.items():
+        if len(options.keys()) == 0:
+            break
+        for k2, v2 in v1.items():
+            parameters[k1][k2] = v2
+
     # Read in all the data
-    data = Data(parameters)
-    data.naive.average()
+    if not isinstance(parameters['Inputs']['feeds'], list):
+        parameters['Inputs']['feeds'] = [parameters['Inputs']['feeds']]
+    if not isinstance(parameters['Inputs']['frequencies'], list):
+        parameters['Inputs']['frequencies'] = [parameters['Inputs']['frequencies']]
+    for band in parameters['Inputs']['bands']:
+        for frequency in parameters['Inputs']['frequencies']:
 
-    #print(np.sum(data.todall))
-    #pyplot.plot(data.todall,',')
-    #pyplot.show()
+            # Data parsing object
+            data = DataLevel2(parameters,band=band,frequency=frequency,keeptod=True)
+            data.naive.average()
 
-    offsetMap, offsets = Destriper(parameters, data)
+
+            offsetMap, offsets = Destriper(parameters, data)
+
+            offsets.average()
+            #pyplot.plot(data.todall-offsets())
+            #pyplot.plot(offsets())
+            #pyplot.show()
+
+            from astropy.io import fits
+            hdu = fits.PrimaryHDU(data.naive()-offsetMap(),header=data.naive.wcs.to_header())
+            hits = fits.ImageHDU(data.hits(returnsum=True), header=data.hits.wcs.to_header())
+            naive   = fits.ImageHDU(data.naive(), header=data.hits.wcs.to_header())
+            offsets = fits.ImageHDU(offsetMap(), header=data.hits.wcs.to_header())
+            
+            hdu1 = fits.HDUList([hdu, hits,naive,offsets])
+            feedstrs = [str(v) for v in parameters['Inputs']['feeds']]
+            hdu1.writeto('fitsfiles/fg4_feeds{}_offset{}_band{}_freq{}.fits'.format('-'.join(feedstrs),
+                parameters['Destriper']['offset'],
+                band,frequency),overwrite=True)
+
+if __name__ == "__main__":
+    call_level1_destripe()
 
     #m = data.naive()
     #m[m == 0] = np.nan
     #pyplot.plot(data.todall)
     #pyplot.plot(np.repeat(offsets.offsets,offsets.offset))
     #pyplot.figure()
-    pyplot.subplot(221)
-    pyplot.imshow(data.naive())
-    pyplot.subplot(222)
-    pyplot.imshow(data.naive()-offsetMap(),vmin=-2e3,vmax=2e3)
-    pyplot.colorbar()
-    pyplot.subplot(223)
-    pyplot.imshow(offsetMap())
+    # pyplot.subplot(221)
+    # pyplot.imshow(data.naive())
+    # pyplot.subplot(222)
+    # pyplot.imshow(data.naive()-offsetMap(),vmin=-2e3,vmax=2e3)
+    # pyplot.colorbar()
+    # pyplot.subplot(223)
+    # pyplot.imshow(offsetMap())
 
-    pyplot.figure()
-    pyplot.imshow(data.naive()-offsetMap(),vmin=0,vmax=1.5e3)
-    pyplot.show()
-
-    from astropy.io import fits
-    hdu = fits.PrimaryHDU(data.naive()-offsetMap(),header=data.naive.wcs.to_header())
-    hits = fits.ImageHDU(data.hits(returnsum=True), header=data.hits.wcs.to_header())
-    naive   = fits.ImageHDU(data.naive(), header=data.hits.wcs.to_header())
-    offsets = fits.ImageHDU(offsetMap(), header=data.hits.wcs.to_header())
-
-    hdu1 = fits.HDUList([hdu, hits,naive,offsets])
-    hdu1.writeto('fg6_all.fits',overwrite=True)
-
-if __name__ == "__main__":
-    call_level1_destripe()
+    # pyplot.figure()
+    # pyplot.imshow(data.naive()-offsetMap(),vmin=0,vmax=1.5e3)
+    # pyplot.show()